263370f3177ff992fe15fd1fba8fec470408575d
alicimen <alicimen1443@gmail.com>
2022-04-21 15:51:12 +0300

    doing final work on hello feature

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,5 +5,9 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        System.out.println("EU8 learning git and github");
+
+
     }
 }
